ac66f974416d43170bdddcda133bedf737c596e5
sio2whocodes <fltcy2039@naver.com>
2025-03-06 23:18:55 +0900

    [sujeong] 2290

--- sujeong/2290.swift
+++ sujeong/2290.swift
@@ -17,104 +17,64 @@ func makeNumChar(_ s:Int) -> [Int:[[Character]]] {
     var numChar:[Int:[[Character]]] = [:]
     
     numChar[0] = [ hline ]
-    for _ in 0..<s {
-        numChar[0]!.append(bothline)
-    }
+    numChar[0]? += Array(repeating: bothline.map{$0}, count: s)
     numChar[0]!.append(emptyhline)
-    for _ in 0..<s {
-        numChar[0]!.append(bothline)
-    }
+    numChar[0]? += Array(repeating: bothline.map{$0}, count: s)
     numChar[0]!.append(hline)
 
     numChar[1] = [ emptyhline ]
-    for _ in 0..<s {
-        numChar[1]!.append(rightline)
-    }
+    numChar[1]? += Array(repeating: rightline.map{$0}, count: s)
     numChar[1]!.append(emptyhline)
-    for _ in 0..<s {
-        numChar[1]!.append(rightline)
-    }
+    numChar[1]? += Array(repeating: rightline.map{$0}, count: s)
     numChar[1]!.append(emptyhline)
     
     numChar[2] = [ hline ]
-    for _ in 0..<s {
-        numChar[2]!.append(rightline)
-    }
+    numChar[2]? += Array(repeating: rightline.map{$0}, count: s)
     numChar[2]!.append(hline)
-    for _ in 0..<s {
-        numChar[2]!.append(leftline)
-    }
+    numChar[2]? += Array(repeating: leftline.map{$0}, count: s)
     numChar[2]!.append(hline)
 
     numChar[3] = [ hline ]
-    for _ in 0..<s {
-        numChar[3]!.append(rightline)
-    }
+    numChar[3]? += Array(repeating: rightline.map{$0}, count: s)
     numChar[3]!.append(hline)
-    for _ in 0..<s {
-        numChar[3]!.append(rightline)
-    }
+    numChar[3]? += Array(repeating: rightline.map{$0}, count: s)
     numChar[3]!.append(hline)
     
 
     numChar[4] = [ emptyhline ]
-    for _ in 0..<s {
-        numChar[4]!.append(bothline)
-    }
+    numChar[4]? += Array(repeating: bothline.map{$0}, count: s)
     numChar[4]!.append(hline)
-    for _ in 0..<s {
-        numChar[4]!.append(rightline)
-    }
+    numChar[4]? += Array(repeating: rightline.map{$0}, count: s)
     numChar[4]!.append(emptyhline)
     
     numChar[5] = [ hline ]
-    for _ in 0..<s {
-        numChar[5]!.append(leftline)
-    }
+    numChar[5]? += Array(repeating: leftline.map{$0}, count: s)
     numChar[5]!.append(hline)
-    for _ in 0..<s {
-        numChar[5]!.append(rightline)
-    }
+    numChar[5]? += Array(repeating: rightline.map{$0}, count: s)
     numChar[5]!.append(hline)
     
     numChar[6] = [ hline ]
-    for _ in 0..<s {
-        numChar[6]!.append(leftline)
-    }
+    numChar[6]? += Array(repeating: leftline.map{$0}, count: s)
     numChar[6]!.append(hline)
-    for _ in 0..<s {
-        numChar[6]!.append(bothline)
-    }
+    numChar[6]? += Array(repeating: bothline.map{$0}, count: s)
     numChar[6]!.append(hline)
     
     numChar[7] = [ hline ]
-    for _ in 0..<s {
-        numChar[7]!.append(rightline)
-    }
+    numChar[7]? += Array(repeating: rightline.map{$0}, count: s)
     numChar[7]!.append(emptyhline)
-    for _ in 0..<s {
-        numChar[7]!.append(rightline)
-    }
+    numChar[7]? += Array(repeating: rightline.map{$0}, count: s)
     numChar[7]!.append(emptyhline)
     
     numChar[8] = [ hline ]
-    for _ in 0..<s {
-        numChar[8]!.append(bothline)
-    }
+    numChar[8]? += Array(repeating: bothline.map{$0}, count: s)
     numChar[8]!.append(hline)
-    for _ in 0..<s {
-        numChar[8]!.append(bothline)
-    }
+    numChar[8]? += Array(repeating: bothline.map{$0}, count: s)
     numChar[8]!.append(hline)
 
     numChar[9] = [ hline ]
-    for _ in 0..<s {
-        numChar[9]!.append(bothline)
-    }
+    numChar[9]? += Array(repeating: bothline.map{$0}, count: s)
     numChar[9]!.append(hline)
-    for _ in 0..<s {
-        numChar[9]!.append(rightline)
-    }
+    numChar[9]? += Array(repeating: rightline.map{$0}, count: s)
     numChar[9]!.append(hline)
     
     return numChar
@@ -129,7 +89,6 @@ func solution() {
     let narr:[Int] = String(n).map { Int(String($0))! }
     
     var result:String = ""
-    
     for r in 0..<2*s+3 {
         for n in narr {
             result += (String((numch[n]?[r] as? [Character])!)+" ")
