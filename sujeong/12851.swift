978b36d9d5330a3b73b0805465dadeaa666d6078
sio2whocodes <fltcy2039@naver.com>
2025-03-31 13:54:22 +0900

    [sujeong] 12851

---
+++ sujeong/12851.swift
@@ -0,0 +1,65 @@
+//
+//  12851.swift
+//  dfsNbfs
+//
+//  Created by sujeong on 3/31/25.
+//
+
+import Foundation
+
+func solution() {
+    let input:[Int] = (readLine()?.split(separator: " ").map { Int($0)! })!
+    let n:Int = input[0]
+    let k:Int = input[1]
+    
+    var dist:[Int] = [Int](repeating: Int.max, count: 100001)
+    var cnt:[Int] = [Int](repeating: 0, count: 100001)
+    
+    dist[n] = 0
+    cnt[n] = 1
+    
+    var q:[Int] = []
+    q.append(n)
+    
+    while !q.isEmpty {
+        let now = q.removeFirst()
+        let nowdist = dist[now]
+        
+        if now-1 >= 0 && now-1 <= 100000 {
+            if dist[now-1] > nowdist+1 {
+                // 처음방문
+                dist[now-1] = nowdist+1
+                cnt[now-1] = cnt[now]
+                q.append(now-1)
+            } else if dist[now-1] == nowdist+1 {
+                cnt[now-1] += cnt[now]
+            } // 방문했는데 dist가 이미 내가 방문할 때보다 짧을때가 없다는 가정임 (bfs니까) - 원래 있던 dist값보다 작은 경우로 덮어써야 할 때가 오지 않음
+        }
+        if now+1 <= 100000 && now+1 >= 0 {
+            if dist[now+1] > nowdist+1 {
+                // 처음방문
+                dist[now+1] = nowdist+1
+                cnt[now+1] = cnt[now]
+                q.append(now+1)
+            } else if dist[now+1] == nowdist+1 {
+                // 이전에 방문한적 있는데 또 최단거리로 방문한 경우
+                cnt[now+1] += cnt[now]
+            }
+        }
+        if now*2 <= 100000 && now*2 >= 0 {
+            if dist[now*2] > nowdist+1 {
+                // 처음방문
+                dist[now*2] = nowdist+1
+                cnt[now*2] = cnt[now]
+                q.append(now*2)
+            } else if dist[now*2] == nowdist+1 {
+                cnt[now*2] += cnt[now]
+            }
+        }
+    }
+    
+    print(dist[k])
+    print(cnt[k])
+}
+
+solution()
